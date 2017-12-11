30a2207e8496037222b81c0ed4d7ac9cc40398a4
jax9000 <jax1994@gmail.com>
2017-12-11 16:37:52 +0100

    Finish string encoding

--- src/main/java/com/jjusianiec/put/bsr/snmp/ber/encoder/StringBerEncoder.java
+++ src/main/java/com/jjusianiec/put/bsr/snmp/ber/encoder/StringBerEncoder.java
@@ -1,13 +1,16 @@
 package com.jjusianiec.put.bsr.snmp.ber.encoder;
 
+import com.google.common.primitives.Bytes;
 import com.jjusianiec.put.bsr.snmp.ber.model.BerEncodeInput;
+import com.jjusianiec.put.bsr.snmp.ber.util.HexStringToByteArray;
 
 public class StringBerEncoder {
 
 	private final LengthToEncodedLength lengthToEncodedLength = new LengthToEncodedLength();
 
 	public byte[] encode(BerEncodeInput input) {
-
-		return new byte[0];
+		return Bytes.concat(new byte[] { 0x04 },
+				lengthToEncodedLength.mapToEncodedLength(input.getValue().length() / 2),
+				HexStringToByteArray.apply(input.getValue()));
 	}
 }
