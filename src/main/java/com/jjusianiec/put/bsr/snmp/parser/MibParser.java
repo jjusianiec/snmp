7b7487cea7854d416f0ad75c2474ca03a7eeaee3
jjusianiec <jjusianiec@gmail.com>
2018-02-22 12:49:16 +0100

    Please don't check this code in recruitment process

--- src/main/java/com/jjusianiec/put/bsr/snmp/parser/MibParser.java
+++ src/main/java/com/jjusianiec/put/bsr/snmp/parser/MibParser.java
@@ -1,23 +1,115 @@
 package com.jjusianiec.put.bsr.snmp.parser;
 
-import java.util.List;
-import java.util.Map;
-
+import com.google.common.collect.Sets;
 import com.jjusianiec.put.bsr.snmp.parser.model.MibTree;
 import com.jjusianiec.put.bsr.snmp.parser.model.OIdRaw;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static java.util.stream.Collectors.joining;
+
 public class MibParser {
+    private final ImportsReader importsReader = new ImportsReader();
+    private final MibFileWithoutCommentsReader mibFileWithoutCommentsReader = new MibFileWithoutCommentsReader();
+    private final OIdReader oIdReader = new OIdReader();
+    private final ObjectTypesReader objectTypeReader = new ObjectTypesReader();
 
-	private final ImportsReader importsReader = new ImportsReader();
-	private final MibFileWithoutCommentsReader mibFileWithoutCommentsReader = new MibFileWithoutCommentsReader();
-	private final OIdReader oIdReader = new OIdReader();
-	private final ObjectTypesReader objectTypeReader = new ObjectTypesReader();
+    public MibTree parseMib(String path) {
+        String content = mibFileWithoutCommentsReader.readFileWithoutComments(path);
+        Map<String, List<String>> fileToImports = importsReader.readImports(content);
+        Map<String, OIdRaw> identifierToOId = oIdReader.readIdentifiers(content);
+        Map<String, OIdRaw> objectTypeIdToOId = objectTypeReader.readObjectTypes(content);
 
-	public MibTree parseMib(String path) {
-		String content = mibFileWithoutCommentsReader.readFileWithoutComments(path);
-		Map<String, List<String>> fileToImports = importsReader.readImports(content);
-		Map<String, OIdRaw> identifierToOId = oIdReader.readIdentifiers(content);
-		Map<String, OIdRaw> objectTypeIdToOId = objectTypeReader.readObjectTypes(content);
-		return null;
-	}
+        String importContent = mibFileWithoutCommentsReader.readFileWithoutComments(getFile(fileToImports));
+        Map<String, OIdRaw> importedIdentifierToOId = oIdReader.readIdentifiers(importContent);
+        List<Map.Entry<String, OIdRaw>> knownOIds = newArrayList();
+        for (Map.Entry<String, OIdRaw> stringOIdRawEntry : importedIdentifierToOId.entrySet()) {
+            OIdRaw value = stringOIdRawEntry.getValue();
+            if (value.getParentIdRaw().split(" ").length > 3) {
+                String[] split = value.getParentIdRaw().replaceAll("iso", ".1.")
+                        .replaceAll("[^\\d.]", ".").split("\\.");
+                value.setParentIdNumber(Stream.of(split)
+                        .filter(s -> s.length() > 0)
+                        .collect(joining("."))
+                );
+                knownOIds.add(stringOIdRawEntry);
+            }
+        }
+        for (int i = 0; i < importedIdentifierToOId.size(); i++) {
+            for (int j = 0; j < importedIdentifierToOId.size(); j++) {
+                for (Map.Entry<String, OIdRaw> stringOIdRawEntry : importedIdentifierToOId.entrySet()) {
+                    if (stringOIdRawEntry.getValue().getParentIdRaw().contains(knownOIds.get(j).getKey())) {
+                        if (!knownOIds.contains(stringOIdRawEntry)) {
+                            knownOIds.add(stringOIdRawEntry);
+                        }
+                        String s = stringOIdRawEntry.getValue().getParentIdRaw().replaceAll("[^\\d.]", "");
+                        stringOIdRawEntry.getValue().setParentIdNumber(knownOIds.get(j).getValue().getParentIdNumber() + "." + s);
+                        if(knownOIds.get(j).getValue().getChildrenObjects() == null) {
+                            knownOIds.get(j).getValue().setChildrenObjects(newHashSet());
+                        }
+                        knownOIds.get(j).getValue().getChildrenObjects().add(stringOIdRawEntry);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < identifierToOId.size(); i++) {
+            for (int j = 0; j < knownOIds.size(); j++) {
+                for (Map.Entry<String, OIdRaw> stringOIdRawEntry : identifierToOId.entrySet()) {
+                    if (stringOIdRawEntry.getValue().getParentIdRaw().contains(knownOIds.get(j).getKey())) {
+                        if (!knownOIds.contains(stringOIdRawEntry)) {
+                            knownOIds.add(stringOIdRawEntry);
+                        }
+                        String s = stringOIdRawEntry.getValue().getParentIdRaw().replaceAll("[^\\d.]", "");
+                        stringOIdRawEntry.getValue().setParentIdNumber(knownOIds.get(j).getValue().getParentIdNumber() + "." + s);
+                        if(knownOIds.get(j).getValue().getChildrenObjects() == null) {
+                            knownOIds.get(j).getValue().setChildrenObjects(newHashSet());
+                        }
+                        knownOIds.get(j).getValue().getChildrenObjects().add(stringOIdRawEntry);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < objectTypeIdToOId.size(); i++) {
+            for (int j = 0; j < knownOIds.size(); j++) {
+                for (Map.Entry<String, OIdRaw> stringOIdRawEntry : objectTypeIdToOId.entrySet()) {
+                    if (stringOIdRawEntry.getValue().getParentIdRaw().contains(knownOIds.get(j).getKey())) {
+                        if (!knownOIds.contains(stringOIdRawEntry)) {
+                            knownOIds.add(stringOIdRawEntry);
+                        }
+                        String s = stringOIdRawEntry.getValue().getParentIdRaw().replaceAll("[^\\d.]", "");
+                        stringOIdRawEntry.getValue().setParentIdNumber(knownOIds.get(j).getValue().getParentIdNumber() + "." + s);
+                        if(knownOIds.get(j).getValue().getChildrenObjects() == null) {
+                            knownOIds.get(j).getValue().setChildrenObjects(newHashSet());
+                        }
+                        knownOIds.get(j).getValue().getChildrenObjects().add(stringOIdRawEntry);
+                    }
+                }
+            }
+        }
+
+        Map.Entry<String, OIdRaw> root = knownOIds.get(0);
+
+
+        System.out.println(objectTypeIdToOId.entrySet().stream()
+                .map(x -> x.getKey() + ":" + x.getValue().getParentIdRaw())
+                .collect(Collectors.toList()).toString().replaceAll(",", "\n"));
+        return null;
+    }
+
+    private String getFile(Map<String, List<String>> fileToImports) {
+        Iterator<Map.Entry<String, List<String>>> iterator = fileToImports.entrySet().iterator();
+        if (iterator.hasNext()) {
+            Map.Entry<String, List<String>> next = iterator.next();
+            return "mibs/" + next.getKey();
+        }
+        return null;
+    }
 }
--- src/main/java/com/jjusianiec/put/bsr/snmp/parser/model/OIdRaw.java
+++ src/main/java/com/jjusianiec/put/bsr/snmp/parser/model/OIdRaw.java
@@ -1,22 +1,29 @@
 package com.jjusianiec.put.bsr.snmp.parser.model;
 
+import com.google.common.collect.Sets;
+import com.jjusianiec.put.bsr.snmp.parser.OIdType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import com.jjusianiec.put.bsr.snmp.parser.OIdType;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OIdRaw {
-	private String parentIdRaw;
-	private String syntax;
-	private String access;
-	private String status;
-	private String description;
-	private String index;
-	private OIdType type;
+    private String parentIdRaw;
+    private String parentIdNumber;
+    private String syntax;
+    private String access;
+    private String status;
+    private String description;
+    private String index;
+    private OIdType type;
+    private Set<Map.Entry<String, OIdRaw>> childrenObjects = newHashSet();
 }
